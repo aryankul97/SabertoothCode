c07698da2785e14d93c7d9e0ea9a49ab606a40df
unknown <shreshtharnd20@gmail.com>
2021-05-13 14:06:49 +0530

    aryan commit

--- SumedhaAPI/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('api/v1/login/',views.login,name='login'),
-    path('api/v1/register/',views.register_user,name='register_user'),
-    path('api/v1/vehicles/',views.vehicles,name='vehicles'),
-    path('api/v1/staff/',views.staff,name='staff'),
-]
